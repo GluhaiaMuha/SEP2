5d5e6dd1a2fd3184e6f4bd75d255b0442d2dec00
GluhaiaMuha <57876096+GluhaiaMuha@users.noreply.github.com>
2022-05-12 23:14:06 +0200

    Update no.5

--- src/client/views/loginView/LoginViewController.java
+++ src/client/views/loginView/LoginViewController.java
@@ -5,9 +5,17 @@ import client.core.ViewHandler;
 import client.core.ViewModelFactory;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class LoginViewController implements ViewController
 {
+  @FXML
+  private TextField emailInput;
+
+  @FXML
+  private PasswordField passwordInput;
+
   private ViewHandler viewHandler;
   private LoginViewModel loginViewModel;
 
@@ -16,6 +24,11 @@ public class LoginViewController implements ViewController
   {
     this.viewHandler = vh;
     loginViewModel = vmf.getLoginViewModel();
+  }
+
+  @FXML
+  void onLoginAction(ActionEvent event) {
+
   }
 
   @FXML
--- src/client/views/registerView/RegisterViewController.java
+++ src/client/views/registerView/RegisterViewController.java
@@ -6,9 +6,26 @@ import client.views.ViewController;
 import client.views.loginView.LoginViewModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class RegisterViewController implements ViewController
 {
+  @FXML
+  private TextField email;
+
+  @FXML
+  private PasswordField password;
+
+  @FXML
+  private TextField phoneNumber;
+
+  @FXML
+  private TextField fName;
+
+  @FXML
+  private TextField sName;
+
   private ViewHandler viewHandler;
   private RegisterViewModel registerViewModel;
 
