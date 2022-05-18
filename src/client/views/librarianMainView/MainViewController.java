f05b5db49fb34a211b124f4c867d80faf187d993
GluhaiaMuha <57876096+GluhaiaMuha@users.noreply.github.com>
2022-05-18 20:58:12 +0200

    Update no.5

--- src/client/views/librarianMainView/MainViewController.java
+++ src/client/views/librarianMainView/MainViewController.java
@@ -4,11 +4,17 @@ import client.core.ViewHandler;
 import client.core.ViewModelFactory;
 import client.views.ViewController;
 import client.views.loginView.LoginViewModel;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableRow;
+
+import javax.swing.text.TableView;
 
 public class MainViewController implements ViewController
 {
   private ViewHandler viewHandler;
   private MainViewModel mainViewController;
+
+  @FXML private TableRow bookTable;
 
   @Override
   public void init(ViewHandler vh, ViewModelFactory vmf)
