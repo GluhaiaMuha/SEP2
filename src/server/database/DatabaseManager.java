df7300b140e7fe98a216b07d681de49a699d21c7
ZoT1YaK <robert.zustars@gmail.com>
2022-05-22 17:51:52 +0200

    Update

--- src/server/database/DatabaseManager.java
+++ src/server/database/DatabaseManager.java
@@ -350,6 +350,101 @@ public class DatabaseManager implements DatabaseInterface{
         }
     }
 
+    public List<Book> readBooksByTitle(String searchString) throws SQLException
+    {
+        try(Connection connection = database.getConnection())
+        {
+            PreparedStatement statement = connection.prepareStatement(
+                "SELECT * FROM book WHERE title LIKE ?");
+            statement.setString(1, "%" + searchString + "%");
+            ResultSet resultSet = statement.executeQuery();
+            ArrayList<Book> books = new ArrayList<>();
+            while (resultSet.next())
+            {
+                String hash = resultSet.getString("hash");
+                String title = resultSet.getString("title");
+                String author = resultSet.getString("author");
+                int pageCount = resultSet.getInt("pageCount");
+                String genre = resultSet.getString("genre");
+                int publication_year = resultSet.getInt("publication_year");
+                int amountInStock = resultSet.getInt("amountInStock");
+                Book book = new Book(hash, title, author, pageCount, genre, publication_year, amountInStock);
+                books.add(book);
+            }
+            return books;
+        }
+    }
+
+    public List<Movie> readMoviesByTitle(String searchString) throws SQLException
+    {
+        try(Connection connection = database.getConnection())
+        {
+            PreparedStatement statement = connection.prepareStatement(
+                "SELECT * FROM movie WHERE title LIKE ?");
+            statement.setString(1, "%" + searchString + "%");
+            ResultSet resultSet = statement.executeQuery();
+            ArrayList<Movie> movies = new ArrayList<>();
+            while (resultSet.next())
+            {
+                String hash = resultSet.getString("hash");
+                String title = resultSet.getString("title");
+                String director = resultSet.getString("director");
+                int release_year = resultSet.getInt("release_year");
+                int length = resultSet.getInt("length");
+                int amountInStock = resultSet.getInt("amountInStock");
+                Movie movie = new Movie(hash, title, director, release_year, length, amountInStock);
+                movies.add(movie);
+            }
+            return movies;
+        }
+    }
+
+    public List<CD> readCDsByTitle(String searchString) throws SQLException
+    {
+        try(Connection connection = database.getConnection())
+        {
+            PreparedStatement statement = connection.prepareStatement(
+                "SELECT * FROM cd WHERE name LIKE ?");
+            statement.setString(1, "%" + searchString + "%");
+            ResultSet resultSet = statement.executeQuery();
+            ArrayList<CD> cds = new ArrayList<>();
+            while (resultSet.next())
+            {
+                String hash = resultSet.getString("hash");
+                String name = resultSet.getString("name");
+                int capacity = resultSet.getInt("capacity");
+                String usage = resultSet.getString("usage");
+                int amountInStock = resultSet.getInt("amountInStock");
+                CD cd = new CD(hash, name, capacity, usage, amountInStock);
+                cds.add(cd);
+            }
+            return cds;
+        }
+    }
+
+    public List<Software> readSoftwaresByTitle(String searchString) throws SQLException
+    {
+        try(Connection connection = database.getConnection())
+        {
+            PreparedStatement statement = connection.prepareStatement(
+                "SELECT * FROM software WHERE name LIKE ?");
+            ResultSet resultSet = statement.executeQuery();
+            ArrayList<Software> softwares = new ArrayList<>();
+            while (resultSet.next())
+            {
+                String hash = resultSet.getString("hash");
+                String name = resultSet.getString("name");
+                String type = resultSet.getString("type");
+                String version = resultSet.getString("version");
+                String license_type = resultSet.getString("license_type");
+                int amountInStock = resultSet.getInt("amountInStock");
+                Software software = new Software(hash, name, type, version, license_type, amountInStock);
+                softwares.add(software);
+            }
+            return softwares;
+        }
+    }
+
     @Override
     public User readUserLogin(String tableName, String email, String password) throws SQLException
     {
